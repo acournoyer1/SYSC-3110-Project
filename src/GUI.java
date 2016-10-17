e6e4f89cd5b451d8f3a3ee77d14249f260e02cb5
alexandrecournoyer <alexandrecournoyer@AA0508-25.labs.sce.carleton.ca>
2016-10-17 15:01:42 -0400

    Initial Push

---
+++ src/GUI.java
@@ -0,0 +1,22 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+
+public class GUI extends JFrame{
+	
+	private JMenuItem addNode;
+	private JMenuItem addConnection;
+	private JMenuItem addMessage;
+	private JMenuItem removeNode;
+	private JMenuItem removeConnection;
+	private JMenuItem viewNode;
+	private JMenuItem viewAverage;
+	private JTextArea statusWindow;
+	private JButton stepButton;
+	
+	public GUI()
+	{
+		
+	}
+}
