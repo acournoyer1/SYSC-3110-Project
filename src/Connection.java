3ddb3c0cf7c0dc7b4c0a1bfa26e8ef7be0ce53e2
acournoyer1 <cournoyer1@gmail.com>
2016-10-17 20:25:22 -0400

    Finished Connection class

--- src/Connection.java
+++ src/Connection.java
@@ -1,4 +1,34 @@
 
 public class Connection {
-
+	private Node firstNode;
+	private Node secondNode;
+	
+	public Connection(Node n1, Node n2)
+	{
+		firstNode = n1;
+		secondNode = n2;
+	}
+	
+	public Node getFirstNode()
+	{
+		return firstNode;
+	}
+	
+	public Node getSecondNode()
+	{
+		return secondNode;
+	}
+	
+	public String toString()
+	{
+		return firstNode.toString() + " < - > " + secondNode.toString();
+	}
+	
+	public boolean equals(Object o)
+	{
+		if(this == o) return true;
+		if(!(o instanceof Connection)) return false;
+		Connection c = (Connection)o;
+		return c.firstNode.equals(this.firstNode) && c.secondNode.equals(this.secondNode);
+	}
 }
