0933a9ee1bba718e1e4bd22656826362cb7c687f
damanveen <damanveen@gmail.com>
2016-10-19 14:56:36 -0400

    Added basic methods for message and node classes

--- src/Message.java
+++ src/Message.java
@@ -3,4 +3,37 @@ public class Message {
 	private Node src;
 	private Node dest;
 	private int count;
+	
+	
+	public Message(){
+		
+	}
+	
+	public Message(Node src, Node dest){
+		
+		
+	}
+	
+	
+	
+	public Node getSrc() {
+		return src;
+	}
+	
+	public void setSrc(Node src) {
+		this.src = src;
+	}
+	
+	public Node getDest() {
+		return dest;
+	}
+	
+	public void setDest(Node dest) {
+		this.dest = dest;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
 }
--- src/Node.java
+++ src/Node.java
@@ -1,7 +1,49 @@
-import java.awt.List;
 import java.util.LinkedList;
 
 public class Node {
 	private String name;
-	private LinkedList<Node> connections;
+	private LinkedList<Connection> connections;
+	
+	
+	
+	public Node(String name){
+		
+		this.name = name;
+		
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public LinkedList<Connection> getConnections() {
+		return connections;
+	}
+
+
+
+	public void setConnections(LinkedList<Connection> connections) {
+		this.connections = connections;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Node [Name = " + name + ", connection 1 = " + connections.getFirst() + " and connection 2 = " + connections.getLast() + "]\n";
+	}
+	
+	
+	
+	
 }
