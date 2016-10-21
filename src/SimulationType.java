e309f65c7b09d39fddcc251b36ec2d2b67a3691e
Ryan Ha <ryanha.mail@gmail.com>
2016-10-21 13:39:36 -0400

    Commenting enum

--- src/SimulationType.java
+++ src/SimulationType.java
@@ -1,4 +1,9 @@
-
+/**
+ * SimulationType enum defines what mode the user has currently selected.
+ *
+ * Created By: Ryan Ha
+ * Last Edited By: 
+ */
 public enum SimulationType {
 	RANDOM,
 	FLOOD,
