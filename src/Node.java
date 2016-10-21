280bc778c5e2763095a839d4b53ba6d28c347238
Ryan Ha <ryanha.mail@gmail.com>
2016-10-21 13:38:03 -0400

    Node comments

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,10 @@
+/**
+ * Node class that holds the properties of every node that will be manipulated.
+ *
+ * Created by: Daman Singh
+ * Last Edited by: Ryan Ha
+ */
+
 import java.util.*;
 
 public class Node {
@@ -12,31 +19,50 @@ public class Node {
 		this.name = name;
 		connections = new HashSet<Node>();
 	}
-
-
-
+	
+	/*
+	 * Get the name of the current node object.
+	 * 
+	 * @ret a string containing the name of the node.
+	 */
 	public String getName() {
 		return name;
 	}
-
-
-
+	
+	/*
+	 * Set the name of the ndoe
+	 *
+	 * @param1 the string that the node will be set to
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-
+	/*
+	 * Get the hashset of nodes that the current node is connected to
+	 *
+	 * @ret a hashset of connections
+	 */
 	public HashSet<Node> getConnections() {
 		return connections;
 	}
-
+	
+	/*
+	 * Connect this node to another node
+	 *
+	 * @param1 the node object that this node will be connected to
+	 */
 	public void connect(Node n)
 	{
 		connections.add(n);
 		n.connections.add(this);
 	}
 	
+	/*
+	 * Disconnect this node from another node
+	 *
+	 * @param2 the node to disconnect from
+	 */
 	public void disconnect(Node n)
 	{
 		connections.remove(n);
@@ -47,6 +73,7 @@ public class Node {
 	/*
 	 * Returns a list of all nodes connected to this node
 	 * 
+	 * @ret a string of the node properties
 	 */
 	public String getDetails()
 	{
@@ -67,8 +94,4 @@ public class Node {
 	public String toString() {
 		return name;
 	}
-	
-	
-	
-	
 }
